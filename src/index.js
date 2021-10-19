599c9f2086caa24dc56ee8f20206846fe233948c
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-10-19 13:53:58 -0300

    Estrutura geral adicionado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+
+const teamRouter = require('./routes/teams.routes')
+const matchesRouter = require('./routes/matches.routes')
+const classificationRoutes = require('./routes/classifications.routes')
+
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => res.send('Hello'))
 
-app.listen(3000)
+app.use('/teams', teamRouter)
+app.use('/matches', matchesRouter)
+app.use('/classification', classificationRoutes)
+
+const port = 2022
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
