2c4e411d209dc5b7a4a996e0944a8b84e0278e4c
Eduardo <54048170+Eduardo-ops@users.noreply.github.com>
2021-10-24 12:00:06 -0300

    Documentacao swagger para team implementado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDoc = require('../swagger.json')
 
 const teamRouter = require('./routes/teams.routes')
 const matchesRouter = require('./routes/matches.routes')
@@ -7,13 +9,15 @@ const classificationRoutes = require('./routes/classifications.routes')
 const app = express()
 app.use(express.json())
 
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
+
 app.get('/', (req, res) => res.send('Hello'))
 
 app.use('/teams', teamRouter)
 app.use('/matches', matchesRouter)
 app.use('/classification', classificationRoutes)
 
-const port = 2022
+const port = 1011
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
